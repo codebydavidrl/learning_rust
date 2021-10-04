66f28355f202080a07e646fc2794c54017854193
David SoD <david@stakeordie.com>
2021-10-04 07:02:21 -0700

    First excersises

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut missiles = 8 ;
+    let ready     = 2 ;
+    println!("Firing {} of my {} missiles...", ready, missiles);
+    missiles = missiles - ready ;
+    println!("{} missiles left", missiles);
+
+}
