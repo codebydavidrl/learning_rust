4c9b5c79bd107277a0a5e7e5cf8f091a42f13ffd
David SoD <david@stakeordie.com>
2021-10-05 16:48:03 -0700

    solve control and flow exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -20,7 +20,13 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
-
+        if arg.to_string() == "sum" {
+            sum();
+        } else if arg.to_string() == "double"{
+            double();
+        } else{
+            count(arg);
+        } 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
     }
@@ -28,6 +34,9 @@ fn main() {
 
 fn sum() {
     let mut sum = 0;
+    for i in 7..=23 {
+        sum  = i + sum;
+    }
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
@@ -42,7 +51,10 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
+    while x <= 500 {
+        x = x*2;
+        count+=1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -51,8 +63,14 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
+    let mut count = 0;
+    loop {
+        if count == 8{
+            break
+        }
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        count+=1;
+    } 
 
     println!(); // This will output just a newline at the end for cleanliness.
 }
